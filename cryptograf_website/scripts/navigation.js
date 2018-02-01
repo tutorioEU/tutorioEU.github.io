40bb2f2c79fd38605ec6eaf44def2dade5221d0c
BreakINbaDs <shapovalromeo@gmail.com>
2018-02-01 13:29:07 +0200

    testing marketing site

---
+++ cryptograf_website/scripts/navigation.js
@@ -0,0 +1,21 @@
+var header = $('#graph-container');
+var range = 200;
+
+$(window).on('scroll', function () {
+
+  var scrollTop = $(this).scrollTop(),
+      height = header.outerHeight(),
+      offset = height / 2,
+      calc = 1 - (scrollTop - offset + range) / range;
+
+  header.css('opacity', calc);
+
+  if (calc > '1') {
+    header.css('opacity', 1);
+    $('header').css('background-color','transparent');
+  } else if ( calc < '0' ) {
+    header.css('opacity', 0);
+    $('header').css('background-color','#373845');
+  }
+
+});
