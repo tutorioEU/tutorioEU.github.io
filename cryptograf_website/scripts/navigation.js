7cc27f7b4b2dff44bf555ca5793a8b47d6c71c99
dimnatkachuk <dimatkachukgf@gmail.com>
2018-02-03 12:50:59 +0200

    Responsive design finished

--- cryptograf_website/scripts/navigation.js
+++ cryptograf_website/scripts/navigation.js
@@ -10,7 +10,7 @@ $(window).on('scroll', function () {
   var header = $('#graph-container');
   var logo = $('#logo');
   var range = 200;
-  var fading = (window.matchMedia("(min-width: 1025px)").matches) ? false : true; 
+  var fading = (window.matchMedia("(min-width: 1025px)").matches) ? false : true;
   var scrollTop = $(this).scrollTop(),
       height = header.outerHeight(),
       offset = height / 2,
@@ -18,35 +18,35 @@ $(window).on('scroll', function () {
 
   header.css('opacity', calc);
   if (fading) {
-    logo.css('opacity', 1 - calc);  
+    logo.css('opacity', 1 - calc);
   }
 
   if (calc > '0.6') {
     header.css('opacity', 1);
     if (fading) {
-      logo.css('opacity', 0);  
+      logo.css('opacity', 0);
     }
     $('header').css('background-color','transparent');
 
   } else if ( calc < '0' ) {
     header.css('opacity', 0);
     if (fading) {
-      logo.css('opacity', 1);  
+      logo.css('opacity', 1);
     }
-    $('header').css('background-color','#373845');
+    $('header').css('background-color','rgba(55,56,69,0.8)');
   }
 
 });
 
 function toggleNavigation() {
   var nav = $('#navigation')
-  var header = $('#header-container')
+  var header = $('header')
   if (nav.css('display') == 'none') {
        nav.slideDown( 700 ).css('display', 'flex');
-       header.css('background-color', 'rgba(55,56,69,0)');
+       header.css('background-color', 'rgba(55,56,69,1)');
   } else {
        nav.slideUp( 700 );
-       header.css('background-color','rgba(55,56,69,0.6');
+       header.css('background-color','rgba(55,56,69,0.8');
      }
   toggleImg();
 }
